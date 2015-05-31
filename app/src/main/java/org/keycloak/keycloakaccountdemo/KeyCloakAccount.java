a1caa59e4541a403009fe108a1120c43391ff6a1
Julio Arrebola <jarrebola@viachannel.com>
2015-06-01 00:20:28 +0200

    Make it work with keycloak 1.1.0 y 1.2.0

--- app/src/main/java/org/keycloak/keycloakaccountdemo/KeyCloakAccount.java
+++ app/src/main/java/org/keycloak/keycloakaccountdemo/KeyCloakAccount.java
@@ -7,20 +7,28 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.jboss.aerogear.android.http.HeaderAndBody;
-import org.jboss.aerogear.android.impl.http.HttpRestProvider;
+//import org.jboss.aerogear.android.http.HeaderAndBody;
+//import org.jboss.aerogear.android.impl.http.HttpRestProvider;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class KeyCloakAccount extends Activity {
@@ -81,10 +89,11 @@ public class KeyCloakAccount extends Activity {
             final Account[] accounts = am.getAccountsByType("org.keycloak.Account");
 
             if (accounts.length == 0) {
-                am.addAccount("org.keycloak.Account", "org.keycloak.Account.token", null, null, getActivity(), null, null);
+                Log.i(this.getClass().getName(), "No account");
+                am.addAccount("org.keycloak.Account", "org.keycloak.Account", null, null, getActivity(), null, null);
             } else {
-
                 Account account = accounts[0];
+                Log.i(this.getClass().getName(),"Account:"+account.toString());
                 fetchAccountInfo(account);
             }
         }
@@ -99,19 +108,56 @@ public class KeyCloakAccount extends Activity {
 
                     URL accountUrl = null;
                     try {
-                        accountUrl = new URL("https://auth-coffeeregister.rhcloud.com/auth/realms/sup/account");
 
-                        Bundle result = am.getAuthToken(account, "org.keycloak.Account.token", null, null, null, null).getResult();
+                        String origUrl;
+                        //origUrl = "http://test.jarrebola.com/WebSocketTest/webresources/authtest";
+                        origUrl="https://wattsec.com/auth/realms/test/account";
+                        accountUrl = new URL(origUrl);
+
+                        Bundle result = am.getAuthToken(account, "org.keycloak.Account", null, getActivity(), null, null).getResult();
                         if (result.containsKey(AccountManager.KEY_ERROR_MESSAGE)) {
                             throw new RuntimeException("Herf derf");
                         } else {
                             String token = result.getString(AccountManager.KEY_AUTHTOKEN);
-                            HttpRestProvider provider = new HttpRestProvider(accountUrl);
-                            provider.setDefaultHeader("Authorization", "bearer " + token);
-                            HeaderAndBody accountData = provider.get();
-                            String accountBody = new String(accountData.getBody());
 
-                            return accountBody;
+                            //HttpRestProvider provider = new HttpRestProvider(accountUrl);
+                            //provider.setDefaultHeader("Authorization", "bearer " + token);
+                            //HeaderAndBody accountData = provider.get();
+                            //String accountBody = new String(accountData.getBody());
+
+                            //return accountBody;
+
+                            HttpURLConnection conn = (HttpURLConnection) accountUrl.openConnection();
+                            conn.setReadTimeout(10000);
+                            conn.setConnectTimeout(15000);
+                            conn.setRequestProperty("Content-Type", "application/json");
+                            conn.setRequestProperty("Accept", "application/json");
+                            conn.setRequestProperty("Authorization", "bearer " + token);
+                            conn.setRequestMethod("GET");
+                            conn.setDoInput(true);
+                            conn.connect();
+
+                            int responseCode = conn.getResponseCode();
+
+                            Log.i(this.getClass().getName(), "ResponseCode:"+responseCode);
+                            StringBuilder salida = new StringBuilder();
+                            if (responseCode != 200) {
+                                salida.append("ResponseCode : ").append(responseCode);
+                            } else {
+                                InputStream stream = conn.getInputStream();
+
+                                Reader reader = null;
+                                reader = new InputStreamReader(stream, "UTF-8");
+
+                                char[] buffer = new char[1];
+                                while (reader.read(buffer, 0, buffer.length) >= 0) {
+                                    salida.append(buffer);
+                                }
+
+                                reader.close();
+                            }
+
+                            return salida.toString();
                         }
 
                     } catch (Exception e) {
@@ -124,6 +170,7 @@ public class KeyCloakAccount extends Activity {
                 @Override
                 protected void onPostExecute(String s) {
                     super.onPostExecute(s);
+                    ((TextView)getActivity().findViewById(R.id.outputText)).setText(s);
                     Toast.makeText(getActivity(), s, Toast.LENGTH_LONG).show();
                 }
             }.execute((Void[]) null);
